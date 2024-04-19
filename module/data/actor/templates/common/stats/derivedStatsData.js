8b0310a30bfbf4f855cac311bd14c0432a846bcc
Stexinator <hurricane90@gmx.de>
2024-04-19 20:57:13 +0200

    fix wrong definitions in data model

--- module/data/actor/templates/common/stats/derivedStatsData.js
+++ module/data/actor/templates/common/stats/derivedStatsData.js
@@ -1,14 +1,14 @@
-import stat from "./statData.js"
+import derivedStat from "./derivedStatData.js";
 
 const fields = foundry.data.fields;
 
 export default function derivedStats() {
     return {
-        hp: new fields.SchemaField(stat("WITCHER.Actor.DerStat.HP")),
-        sta: new fields.SchemaField(stat("WITCHER.Actor.DerStat.Sta")),
-        resolve: new fields.SchemaField(stat("WITCHER.Actor.DerStat.Resolve")),
-        focus: new fields.SchemaField(stat("WITCHER.Actor.DerStat.Focus")),
-        vigor: new fields.SchemaField(stat("WITCHER.Actor.DerStat.Vigor")),
+        hp: new fields.SchemaField(derivedStat("WITCHER.Actor.DerStat.HP")),
+        sta: new fields.SchemaField(derivedStat("WITCHER.Actor.DerStat.Sta")),
+        resolve: new fields.SchemaField(derivedStat("WITCHER.Actor.DerStat.Resolve")),
+        focus: new fields.SchemaField(derivedStat("WITCHER.Actor.DerStat.Focus")),
+        vigor: new fields.SchemaField(derivedStat("WITCHER.Actor.DerStat.Vigor")),
         modifiersIsOpened: new fields.BooleanField({initial: false}),
     };
   }
