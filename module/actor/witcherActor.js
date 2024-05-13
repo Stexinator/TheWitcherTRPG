3b3f12c9739dc746575df11fe8803dc9e60f1894
Stexinator <hurricane90@gmx.de>
2024-05-13 10:55:27 +0200

    first set of mixins

--- module/actor/witcherActor.js
+++ module/actor/witcherActor.js
@@ -66,7 +66,7 @@ export default class WitcherActor extends Actor {
   }
 
   getList(name) {
-      return this.items.filter(i => i.type == name && !i.system.isStored)
+    return this.items.filter(i => i.type == name && !i.system.isStored)
   }
 
   // Find needed component in the items list based on the component name or based on the exact name of the substance in the players compendium
@@ -77,29 +77,29 @@ export default class WitcherActor extends Actor {
   // which will indicate whether the component is substance or not.
   // Such modification may require either modification dozens of compendiums, or some additional parsers
   findNeededComponent(componentName) {
-    return this.items.filter(item => 
-       item.type == "component" &&
-        (item.name == componentName ||
-          (item.system.type == "substances" &&
-            ((game.i18n.localize("WITCHER.Inventory.Vitriol") == componentName
-              && item.system.substanceType == "vitriol") ||
-              (game.i18n.localize("WITCHER.Inventory.Rebis") == componentName
-                && item.system.substanceType == "rebis") ||
-              (game.i18n.localize("WITCHER.Inventory.Aether") == componentName
-                && item.system.substanceType == "aether") ||
-              (game.i18n.localize("WITCHER.Inventory.Quebrith") == componentName
-                && item.system.substanceType == "quebrith") ||
-              (game.i18n.localize("WITCHER.Inventory.Hydragenum") == componentName
-                && item.system.substanceType == "hydragenum") ||
-              (game.i18n.localize("WITCHER.Inventory.Vermilion") == componentName
-                && item.system.substanceType == "vermilion") ||
-              (game.i18n.localize("WITCHER.Inventory.Sol") == componentName
-                && item.system.substanceType == "sol") ||
-              (game.i18n.localize("WITCHER.Inventory.Caelum") == componentName
-                && item.system.substanceType == "caelum") ||
-              (game.i18n.localize("WITCHER.Inventory.Fulgur") == componentName
-                && item.system.substanceType == "fulgur"))))
-        );
+    return this.items.filter(item =>
+      item.type == "component" &&
+      (item.name == componentName ||
+        (item.system.type == "substances" &&
+          ((game.i18n.localize("WITCHER.Inventory.Vitriol") == componentName
+            && item.system.substanceType == "vitriol") ||
+            (game.i18n.localize("WITCHER.Inventory.Rebis") == componentName
+              && item.system.substanceType == "rebis") ||
+            (game.i18n.localize("WITCHER.Inventory.Aether") == componentName
+              && item.system.substanceType == "aether") ||
+            (game.i18n.localize("WITCHER.Inventory.Quebrith") == componentName
+              && item.system.substanceType == "quebrith") ||
+            (game.i18n.localize("WITCHER.Inventory.Hydragenum") == componentName
+              && item.system.substanceType == "hydragenum") ||
+            (game.i18n.localize("WITCHER.Inventory.Vermilion") == componentName
+              && item.system.substanceType == "vermilion") ||
+            (game.i18n.localize("WITCHER.Inventory.Sol") == componentName
+              && item.system.substanceType == "sol") ||
+            (game.i18n.localize("WITCHER.Inventory.Caelum") == componentName
+              && item.system.substanceType == "caelum") ||
+            (game.i18n.localize("WITCHER.Inventory.Fulgur") == componentName
+              && item.system.substanceType == "fulgur"))))
+    );
   }
 
   async removeItem(itemId, quantityToRemove) {
